ed6604d571c13b0ba9e7411a0425fed1e32bc149
Rebekah Figueroa <figueroarebekah@gmail.com>
2022-05-15 16:15:51 -0500

    cat bot

---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+export const Keys = async () => {
+  const file = process.env.NODE_ENV === "production" ? "./prod.js" : "./dev.js";
+
+  const module = await import(file);
+  return module.keys;
+};
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { Client } from "discord.js";
+
+import { Keys } from "./config/index.js";
+
+const keys = await Keys();
+console.log(keys);
+
+const client = new Client();
+
+client.login(keys.DISCORD_TOKEN);
+
+client.on("message", async (message) => {
+  if (message.content === "!cat") {
+    message.channel.send(`https://cataas.com/cat?t=${new Date().getTime()}`);
+  }
+});
